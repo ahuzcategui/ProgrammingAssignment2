85a9f061b1e8730b5c721e13daa674223b7d0430
ahuzcategui <ahua1111@gmail.com>
2019-05-03 12:51:05 -0400

    This is the programming assignment #2 for the Johm Hopkins Data Science course.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## these functions cache the inverse of a matrix(square) and then retrieve it
 
-## Write a short comment describing this function
+## to be honest i don't fully understand this function nor the assignment, but this works as the one with the vectors
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+    x <<- y
+    inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) setinv <<- solve
+    getinv <- function() inv
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
 }
